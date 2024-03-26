82ae8b11792e424448f0461e2412ce701a67ad90
yuwenjuan <yuwenjuan@bigai.ai>
2024-03-26 10:20:08 +0800

    build first

---
+++ _next/static/chunks/app/posts/[id]/page-39ca151ee35a8f04.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[722],{4267:function(n,t,u){Promise.resolve().then(u.bind(u,7323))},7323:function(n,t,u){"use strict";u.r(t),u.d(t,{Button:function(){return e.ZP}});var e=u(5506)}},function(n){n.O(0,[580,506,971,69,744],function(){return n(n.s=4267)}),_N_E=n.O()}]);
